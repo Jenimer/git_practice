b40423c19cb3b912eca5df6a777528e7ba19aa23
Jen Broadhead <jeniffer.broadhead@gmail.com>
2018-05-10 12:05:05 -0600

    move git on console

--- main.rb
+++ main.rb
@@ -1,26 +1,25 @@
 require 'colorize'
+require_relative 'git'
 
-def puts_git(cmd)
-  puts `git #{cmd} -h`
+class MAIN
+    include Git
+    def self.menu
+        puts 'MAIN MENU'.colorize(:cyan)
+        puts '1: Enter git command'.colorize(:cyan)
+        puts '2: Exit'.colorize(:cyan)
+        choice = gets.to_i
+
+        case choice
+            when 1
+            puts 'Enter a git command'.colorize(:green)
+           Git.puts_git(gets.strip)
+            menu
+            when 2
+            exit
+            else
+            puts 'Invalid Choice'.colorize(:red)
+            menu
+        end
+    end
 end
-
-def menu
-  puts 'MAIN MENU'.colorize(:cyan)
-  puts '1: Enter git command'.colorize(:cyan)
-  puts '2: Exit'.colorize(:cyan)
-  choice = gets.to_i
-
-  case choice
-    when 1
-      puts 'Enter a git command'.colorize(:green)
-      puts_git(gets.strip)
-      menu
-    when 2
-      exit
-    else
-      puts 'Invalid Choice'.colorize(:red)
-      menu
-  end
-end
-
-menu
+Main.menu
